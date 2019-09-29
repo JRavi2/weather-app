1b0f32d4e097589eb3c67defd7a0ba514383f6e8
Ravi Jain <jravi248@gmail.com>
2019-09-29 16:40:32 +0530

    Added Autocomplete feature

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,12 +1,15 @@
 const request = require("request");
 
 const geocode = (address, callback) => {
-    const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+ encodeURIComponent(address) +".json?access_token=pk.eyJ1IjoianJhdmkyNDgiLCJhIjoiY2p6ZWM2N2k2MDB4YTNocWZpY3pzaWxmdiJ9.Enc1s0Mal1UmnYm-tJqqyA&limit=1";
+    const url =
+        "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+        encodeURIComponent(address) +
+        ".json?access_token=pk.eyJ1IjoianJhdmkyNDgiLCJhIjoiY2p6ZWM2N2k2MDB4YTNocWZpY3pzaWxmdiJ9.Enc1s0Mal1UmnYm-tJqqyA&limit=1";
 
-    request({ url, json : true }, (error, { body: locationData } = {}) => {
-        if(error) {
+    request({ url, json: true }, (error, { body: locationData } = {}) => {
+        if (error) {
             callback("Unable to connect to Location Services", undefined);
-        } else if(locationData.features.length === 0) {
+        } else if (locationData.features.length === 0) {
             callback("Unable to find the Location", undefined);
         } else {
             const data = {
